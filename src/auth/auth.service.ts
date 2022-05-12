e1f602c32bfc4da03dd014babc866cf77677bebd
trungphuc94 <trungphuc94@gmail.com>
2022-05-12 14:02:48 +0700

    sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from 'argon2'
+import { AuthDTO } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return { msg: 'I have signin' }
+  constructor(private prisma: PrismaService) { }
+
+  async signup(dto: AuthDTO) {
+    // generate the password
+    const hash = await argon.hash(dto.password)
+    // save the new user in the DB
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      })
+      delete user.hash
+
+      return user
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code == 'P2002') {
+          throw new ForbiddenException('Credentials taken!')
+        }
+      }
+      throw err
+    }
+
   }
 
-  signup() {
-    return { msg: 'I have signup' }
+  signin() {
+    return { msg: 'I have signin' }
   }
 }
