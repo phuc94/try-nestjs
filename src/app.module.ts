ba62fa8e6b1273c40cb7a4761a8026a9f0b653b8
trungphuc94 <trungphuc94@gmail.com>
2022-05-12 23:18:40 +0700

    add jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module.js';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module.js'
+import { UserModule } from './user/user.module'
+import { BookmarkModule } from './bookmark/bookmark.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,21 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 import * as argon from 'argon2'
-import { AuthDTO } from "./dto";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { AuthDTO } from './dto'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) { }
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDTO) {
     // generate the password
@@ -16,24 +25,71 @@ export class AuthService {
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
-          hash
-        }
+          hash,
+        },
       })
-      delete user.hash
-
-      return user
+      return this.signToken(user.id, user.email)
     } catch (err) {
-      if (err instanceof PrismaClientKnownRequestError) {
+      if (
+        err instanceof
+        PrismaClientKnownRequestError
+      ) {
         if (err.code == 'P2002') {
-          throw new ForbiddenException('Credentials taken!')
+          throw new ForbiddenException(
+            'Credentials taken!',
+          )
         }
       }
       throw err
     }
-
   }
 
-  signin() {
-    return { msg: 'I have signin' }
+  async signin(dto: AuthDTO) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      })
+    // user not existed -> throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Credential incorrect!',
+      )
+    // check password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    )
+    // not correct -> throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect!',
+      )
+    // send back the user
+    return this.signToken(user.id, user.email)
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    }
+    const secret = this.config.get('JWT_SECRET')
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret,
+      },
+    )
+
+    return {
+      access_token: token,
+    }
   }
 }
